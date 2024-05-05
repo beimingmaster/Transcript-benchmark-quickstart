e5b80e0fc0876f8b09aa224123a61f3f13ec89e6
xiaoming <lininruc@gmail.com>
2024-05-05 15:03:41 +0800

    add mvs

--- app-tt.py
+++ app-tt.py
@@ -1,3 +1,5 @@
+## 第二次加工(从result-cn之后...)
+
 from openai import OpenAI
 import os
 os.environ["OPENAI_API_KEY"] = "sk-"
@@ -5,7 +7,7 @@ os.environ["OPENAI_API_KEY"] = "sk-"
 def askGPT3(mprompt):
     sprompt = """
 重写整理内容为更易于中国人阅读的样子，包括段落修正、句子逻辑修正、翻译不通顺的之处重新表达意译，注意输出忠于原文的完整内容，保持原文意思不变，并且满足以下条件:
-- 以下是常见的 AI 相关术语词汇对应表（English -> 中文）：
+- 遵循以下常见的 AI 相关术语词汇对应表(原文 -> 重写后输出）：
   * Transformer -> Transformer
   * Token -> Token
   * LLM/Large Language Model -> 大语言模型
@@ -14,6 +16,15 @@ def askGPT3(mprompt):
   * AI Agent -> AI 智能体
   * AGI -> 通用人工智能
   * 法学硕士 -> 大语言模型
+  * 快速工程 -> prompt工程
+  * 即时工程 -> prompt工程
+  * 检索增强生成 -> 检索增强生成（RAG）
+  * 令牌 -> token
+  * RAG -> 检索增强生成（RAG）
+  * 微调 -> fine-tuning
+  * 涡轮增压 -> Turbo
+  * 性能 -> 效果
+
     """
     client = OpenAI()
     completion = client.chat.completions.create(
@@ -25,21 +36,16 @@ def askGPT3(mprompt):
     )
     result = completion.choices[0].message.content.strip()
     print(result)
+    print()
     return(result)
 
 
-with open("/Users/a0000/mywork/commonLLM/opensource/nnnew/Transcript-benchmark-quickstart/dataset-cn/sam-22.txt", 'r') as file:
+with open("/Users/a0000/mywork/commonLLM/opensource/nnnew/Transcript-benchmark-quickstart/temp-cn/openai-t.txt", 'r') as file:
     # 逐行读取内容
     for line in file:
-        # 打印每一行的内容（去除末尾的换行符）
-        if line.startswith("("):
-            line = line.split(") ")[1].strip()
+        if len(line) > 2:
             askGPT3(line)
             print()
-
-
-            # print(line.strip())
-        
 
 
 
